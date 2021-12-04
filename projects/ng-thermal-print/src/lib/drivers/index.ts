ebb6b92f0e65d2560914a4be932e560815b95477
Lucchetto <lucchetto.tie@live.com>
2021-12-04 14:24:24 +0100

    Drop StarPRNT support
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- projects/ng-thermal-print/src/lib/drivers/index.ts
+++ projects/ng-thermal-print/src/lib/drivers/index.ts
@@ -1,6 +1,10 @@
-import { WebPrintDriver } from './WebPrintDriver';
+/**
+ * @Author: Zhenxiang Chen
+ * @Date:   2021-11-24 22:01:46
+ * @Last Modified by:   Zhenxiang Chen
+ * @Last Modified time: 2021-12-04 14:22:08
+ */
 import { UsbDriver } from './UsbDriver';
 export {
     UsbDriver,
-    WebPrintDriver
 }
